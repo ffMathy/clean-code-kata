20b7b1ed03f2dbdd8407508159cd410009eb9e54
Mathias Lorenzen <mathias.lorenzen@live.com>
2022-06-14 04:07:11 +0200

    First draft.

---
+++ src/CleanCodeKata/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Hello, World!");
+
+/*
+
+- readability
+    - extracting classes
+        - too many method arguments
+        - too many methods
+    - extracting methods
+        - cyclomatic complexity
+    - extracting variables
+        - complicated if-statement
+        - magic numbers
+    - DRY
+        - over-use
+        - under-use
+- design
+    - single responsibility
+        - high coupling
+        - low cohesion
+    - YAGNI
+        - over-abstraction
+    - design patterns
+        - over-use
+        - under-use
+
+*/
